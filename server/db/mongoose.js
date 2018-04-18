4b72161cae51db36ee6534dbec2b2cee4d132179
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-19 00:02:34 +0530

    Set up Test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI)
 // mongoose.connect('mongodb://localhost:27017/TodoApp')
 
-mongoose.connect('mongodb://Poorva:P123456@ds249299.mlab.com:49299/my-node-api-server-db');
+// mongoose.connect('mongodb://Poorva:P123456@ds249299.mlab.com:49299/my-node-api-server-db');
 
 module.exports.mongoose = mongoose;
