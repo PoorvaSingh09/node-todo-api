d31a4a9e765776df22ef154478ec73b6256b0ad7
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-18 22:49:29 +0530

    Adding tests for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -56,11 +56,11 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((result) => {
-    if(!result) {
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
       return res.status(404).send();
     }
-    res.send(result);
+    res.send({todo});
   }, (err) => {
     res.status(400).send(err);
   });
