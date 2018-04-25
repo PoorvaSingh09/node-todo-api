1326a774095966b2ec9b71d769e2fed567b04640
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-25 16:11:32 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env *******', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-} else {
-  process.env.MONGODB_URI = 'mongodb://Poorva:P123456@ds249299.mlab.com:49299/my-node-api-server-db'
-}
+if(env === 'test' || env === 'development') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+} 
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -13,7 +13,7 @@ const users = [{
   password: 'user1Pass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc').toString()
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }, {
   _id: userTwoId,
@@ -21,7 +21,7 @@ const users = [{
   password: 'user2Pass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc').toString()
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }];
 
